54ebdeaa42a1fe7d0fb2c0a8bd4c324230e88c1f
Siavash Ghassemi <dersia@web.de>
2017-09-19 07:36:19 -0700

    added FunctionsHttpClient. Added static PassthroughAuth to pass through auth information

---
+++ src/FunctionsHttpClient/AuthPassthroughHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FunctionsHttpClient
+{
+    public class AuthPassthroughHandler : DelegatingHandler
+    {
+        private AuthenticationHeaderValue _authHeader;
+        private bool _overrideAuth = false;
+
+        public AuthPassthroughHandler(AuthenticationHeaderValue authHeader, bool overrideAuth = false, HttpMessageHandler innerHandler = null) : base (innerHandler)
+        {
+            _authHeader = authHeader;
+            _overrideAuth = overrideAuth;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (_authHeader != null && (request.Headers.Authorization != null || _overrideAuth))
+            {
+                request.Headers.Authorization = _authHeader;
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
